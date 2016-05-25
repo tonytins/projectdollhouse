86cdd58a81feefc8d2b577225949d037e4810e07
Mats Vederhus <afr0_at_afr0games.com>
2016-05-25 22:59:10 +0200

    Added HIT definitions.

---
+++ TSOClient/Files/Files/AudioLogic/HITDefinitions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Files.AudioLogic
+{
+    public enum HITPerson
+    {
+        Instance = 0x0,
+        Gender = 0x1
+    }
+}
